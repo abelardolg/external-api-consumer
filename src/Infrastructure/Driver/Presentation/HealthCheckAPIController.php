4605628cd4d7f4e08887a7e7a270cffa5c0c370b
Tu Nombre <you@example.com>
2023-04-26 11:43:51 +0200

    Test HealthCheckApiController

--- src/Infrastructure/Driver/Presentation/HealthCheckAPIController.php
+++ src/Infrastructure/Driver/Presentation/HealthCheckAPIController.php
@@ -10,8 +10,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HealthCheckAPIController
 {
+    const API_UP_AND_RUNNING = "API up and running!";
     public function __invoke(): Response
     {
-        return new JsonResponse(['message' => 'API up and running!']);
+        return new JsonResponse(['message' => self::API_UP_AND_RUNNING]);
     }
 }
